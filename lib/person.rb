2a7af71da15083c6c27396495c64360c03304a6d
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-06 16:40:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,7 @@
 class Person
   def initialize(name)
     @name = name
+    @breed = breed
 end
 
   def name=(name)
@@ -12,6 +13,15 @@ end
     @name
   end
     #getter
+
+    def breed=(breed)
+      @breed = breed
+    end
+
+    def breed
+      @breed
+    end
+
 end
 
 Beyonce = Person.new("Beyonce")
