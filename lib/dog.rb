61269c62ab91dd0188d4b8524f1ffb2e6fc58f50
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-06 16:38:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 class Dog
   def initialize(name)
     @name = name
-  end
+end
 
   def name=(name)
     @name = name
   end
+#setter
 
   def name
     @name
   end
+    #getter
 end
 
   Fido = Dog.new("Fido")
