00b05f5ebbce06e2ec209e031351ba8ef7b595d1
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-06 16:31:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,17 @@
 class Person
-
+  def initialize(name)
+    @name = name
 end
 
+  def name=(name)
+    @name = name
+  end
+#setter
+
+  def name
+    @name
+  end
+    #getter
+end
+    
 Beyonce = Person.new
